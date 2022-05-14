b31939e904b3efa1699f243871a3c17339474c7d
Janis Neibergs <neibis21@gmail.com>
2022-05-14 11:33:19 +0300

    Initial commit

---
+++ src/test/java/com/example/flightplanner/FlightPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
